d198c6671228b673dd3b6c3713e71cd7c7df7100
mateusjunges <mateus@junges.dev>
2021-05-06 03:43:19 -0300

    wip

---
+++ src/Providers/PixServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Junges\Pix\PixServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class PixServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
