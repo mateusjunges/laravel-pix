61b2f1bd78ca68addf5e06179bc04a70cde675c0
mateusjunges <mateus@junges.dev>
2021-05-09 00:39:24 -0300

    add missing methods to the facade

--- src/Facades/Api.php
+++ src/Facades/Api.php
@@ -4,6 +4,7 @@ namespace Junges\Pix\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Junges\Pix\Api\Api as PixApi;
+use Junges\Pix\Api\ApiRequest;
 
 /**
  * Class Api
@@ -12,6 +13,9 @@ use Junges\Pix\Api\Api as PixApi;
  * @method static PixApi clientId(string $clientId);
  * @method static PixApi clientSecret(string $clientSecret);
  * @method static PixApi certificate(string $certificate);
+ * @method static mixed getOauth2Token();
+ * @method static array createCob(ApiRequest $request);
+ * @method static array getCobInfo(string $transaction_id);
  */
 class Api extends Facade
 {
