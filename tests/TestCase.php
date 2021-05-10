f30740ad993b5bbe6d6959083502541d208927e8
mateusjunges <mateus@junges.dev>
2021-05-10 15:18:57 -0300

    tests

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Junges\Pix\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+}
