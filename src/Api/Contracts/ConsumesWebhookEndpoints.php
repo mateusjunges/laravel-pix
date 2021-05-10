d6e5c22ac65ef4313a39d32d842586c342128d58
mateusjunges <mateus@junges.dev>
2021-05-10 11:54:13 -0300

    Changed method name

--- src/Api/Contracts/ConsumesWebhookEndpoints.php
+++ src/Api/Contracts/ConsumesWebhookEndpoints.php
@@ -6,7 +6,7 @@ interface ConsumesWebhookEndpoints extends ConsumesPixApi
 {
     public function create(string $pixKey): array;
 
-    public function getViaPixKey(string $pixKey): array;
+    public function getByPixKey(string $pixKey): array;
 
     public function delete(string $pixKey): array;
 
