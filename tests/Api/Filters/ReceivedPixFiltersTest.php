1b5afc4a7b00adbc15b476f5dd7ef1ffa0e681ba
mateusjunges <mateus@junges.dev>
2021-05-12 18:49:54 -0300

    Finished filters test

---
+++ tests/Api/Filters/ReceivedPixFiltersTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Junges\Pix\Tests\Api\Filters;
+
+use Illuminate\Support\Str;
+use Junges\Pix\Api\Filters\ReceivedPixFilters;
+use Junges\Pix\Exceptions\ValidationException;
+use Junges\Pix\Tests\TestCase;
+
+class ReceivedPixFiltersTest extends TestCase
+{
+    public function test_it_return_filters_in_the_correct_format()
+    {
+        $expected = [
+            'inicio' => $start = now()->subMonth()->toISOString(),
+            'fim' => $end = now()->subMonth()->toISOString(),
+            "txIdPresente" => "false",
+            'cpf' => "12345678900",
+            'paginacao.itensPorPagina' => 2,
+            'paginacao.paginaAtual' => 1,
+            'txid' => $txid = Str::random()
+        ];
+
+        $filters = (new ReceivedPixFilters())
+            ->startingAt($start)
+            ->withoutTransactionIdPresent()
+            ->currentPage(1)
+            ->transactionId($txid)
+            ->itemsPerPage(2)
+            ->cpf('12345678900')
+            ->endingAt($end);
+
+        $this->assertEquals($expected, $filters->toArray());
+    }
+
+    public function test_it_throws_exception_if_start_or_end_are_empty()
+    {
+        $this->expectException(ValidationException::class);
+
+        (new ReceivedPixFilters())->toArray();
+    }
+}
---
+++ tests/Api/Filters/WebhookFiltersTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Junges\Pix\Tests\Api\Filters;
+
+use Junges\Pix\Api\Filters\WebhookFilters;
+use Junges\Pix\Tests\TestCase;
+
+class WebhookFiltersTest extends TestCase
+{
+    public function test_it_return_filters_in_the_correct_format()
+    {
+        $expected = [
+            'inicio' => $start = now()->subMonth()->toISOString(),
+            'fim' => $end = now()->subMonth()->toISOString(),
+            'paginacao.itensPorPagina' => 2,
+            'paginacao.paginaAtual' => 1,
+        ];
+
+        $filters = (new WebhookFilters())
+            ->startingAt($start)
+            ->currentPage(1)
+            ->itemsPerPage(2)
+            ->endingAt($end);
+
+        $this->assertEquals($expected, $filters->toArray());
+    }
+}
