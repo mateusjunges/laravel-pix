a46f59506a72c2626370bc475b33e396c385d14d
mateusjunges <mateus@junges.dev>
2021-05-12 15:40:16 -0300

    Added receivedPix tests

--- tests/Api/Resources/ReceivedPix/ReceivedPixTest.php
+++ tests/Api/Resources/ReceivedPix/ReceivedPixTest.php
@@ -2,12 +2,110 @@
 
 namespace Junges\Pix\Tests\Api\Resources\ReceivedPix;
 
+use Illuminate\Support\Facades\Http;
+use Junges\Pix\Api\Filters\ReceivedPixFilters;
 use Junges\Pix\Exceptions\ValidationException;
 use Junges\Pix\Pix;
 use Junges\Pix\Tests\TestCase;
 
 class ReceivedPixTest extends TestCase
 {
+    public function test_it_can_get_a_pix_by_e2eid()
+    {
+        Http::fake([
+            'pix.example.com/v2/*' => Http::response($response = [
+                'endToEndId' => 'E12345678202009091221abcdef12345',
+                'txid' => 'cd1fe328c875481285a6f233ae41b662',
+                'valor' => '100.00',
+                'horario' => '2020-09-10T13:03:33.902Z',
+                'infoPagador' => 'Reforma da casa',
+                'devolucoes' => [
+                    0 => [
+                        'id' => '000AAA111',
+                        'rtrId' => 'D12345678202009091000abcde123456',
+                        'valor' => '11.00',
+                        'horario' => [
+                            'solicitacao' => '2020-09-10T13:03:33.902Z',
+                        ],
+                        'status' => 'EM_PROCESSAMENTO',
+                    ],
+                ],
+            ])
+        ]);
+
+        $pix = Pix::receivedPix()->getBye2eid('E12345678202009091221abcdef12345');
+
+        $this->assertTrue($pix->successful());
+        $this->assertEquals($response, $pix->json());
+    }
+
+    public function test_it_can_issue_refunds()
+    {
+        Http::fake([
+            'pix.example.com/v2/*' => Http::response($response = ['valor' => '7.89'])
+        ]);
+
+        $pix = Pix::receivedPix()->refund('E12345678202009091221abcdef12345', '123456');
+
+        $this->assertTrue($pix->successful());
+        $this->assertEquals($response, $pix->json());
+    }
+
+    public function test_it_can_consult_a_issued_refund()
+    {
+        Http::fake([
+            'pix.example.com/v2/*' => Http::response($response = [
+                'id' => '123456',
+                'rtrId' => 'D12345678202009091000abcde123456',
+                'valor' => '7.89',
+                'horario' => [
+                    'solicitacao' => '2020-09-11T15:25:59.411Z',
+                ],
+                'status' => 'EM_PROCESSAMENTO',
+            ])
+        ]);
+
+        $pix = Pix::receivedPix()->consultRefund('E12345678202009091221abcdef12345', '123456');
+
+        $this->assertTrue($pix->successful());
+        $this->assertSame($response, $pix->json());
+    }
+
+    public function test_it_can_get_all_pix()
+    {
+        Http::fake([
+            'pix.example.com/v2/*' => Http::response($response = [
+                'parametros' => [
+                    'inicio' => '2020-04-01T00:00:00Z',
+                    'fim' => '2020-04-01T23:59:59Z',
+                    'paginacao' => [
+                        'paginaAtual' => 0,
+                        'itensPorPagina' => 100,
+                        'quantidadeDePaginas' => 1,
+                        'quantidadeTotalDeItens' => 2,
+                    ],
+                ],
+                'pix' => [
+                    0 => [
+                        '$ref' => 'openapi.yaml#/components/examples/pixResponse1/value',
+                    ],
+                    1 => [
+                        '$ref' => 'openapi.yaml#/components/examples/pixResponse2/value',
+                    ],
+                ],
+            ])
+        ]);
+
+        $filters = (new ReceivedPixFilters())
+            ->startingAt(now()->subMonth()->toISOString())
+            ->endingAt(now()->addMonth()->toISOString());
+
+        $pix = Pix::receivedPix()->withFilters($filters)->all();
+
+        $this->assertTrue($pix->successful());
+        $this->assertEquals($response, $pix->json());
+    }
+
     public function test_it_throws_validation_exception_if_filters_are_not_set()
     {
         $this->expectException(ValidationException::class);
