ea213a24ceb4141961e715d7b37d8e6f0e841a1f
mateusjunges <mateus@junges.dev>
2021-05-12 18:31:56 -0300

    Cob filters tests

--- tests/Api/Filters/CobFiltersTest.php
+++ tests/Api/Filters/CobFiltersTest.php
@@ -6,17 +6,27 @@ use Junges\Pix\Api\Filters\CobFilters;
 use Junges\Pix\Exceptions\ValidationException;
 use Junges\Pix\Tests\TestCase;
 
-class CobFilterTest extends TestCase
+class CobFiltersTest extends TestCase
 {
     public function test_it_return_filters_in_the_correct_format()
     {
         $expected = [
             'inicio' => $start = now()->subMonth()->toISOString(),
             'fim' => $end = now()->subMonth()->toISOString(),
+            'locationPresente' => "false",
+            'cpf' => '12345678900',
+            'status' => 'ATIVA',
+            'paginacao.itensPorPagina' => 2,
+            'paginacao.paginaAtual' => 1
         ];
 
         $filters = (new CobFilters())
             ->startingAt($start)
+            ->withoutLocationPresent()
+            ->cpf('12345678900')
+            ->withStatus('ATIVA')
+            ->currentPage(1)
+            ->itemsPerPage(2)
             ->endingAt($end);
 
         $this->assertSame($expected, $filters->toArray());
@@ -26,6 +36,6 @@ class CobFilterTest extends TestCase
     {
         $this->expectException(ValidationException::class);
 
-        $filters = (new CobFilters())->toArray();
+        (new CobFilters())->toArray();
     }
 }
